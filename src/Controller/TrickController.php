d075786b7086a51b30a53bac299f8eb7ca5eb74f
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-06-12 21:54:01 +0200

    adding the form to add a message on the view of a trick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -9,12 +9,14 @@
 namespace App\Controller;
 
 
+use App\Entity\Chat;
 use App\Entity\Trick;
+use App\Form\ChatType;
 use App\Repository\ChatRepository;
 use App\Repository\TrickRepository;
-use App\Repository\MediaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -59,13 +61,34 @@ class TrickController extends  AbstractController
      * Affichage le detail d'une figure
      * @Route("Figures/{id}/{page}", name="trick.show")
      * @param Trick $trick
+     * @param $page
+     * @param ChatRepository $chatRepository
+     * @param Request $request
      * @return Response
      */
-    public function show(Trick $trick, $page, ChatRepository $chatRepository) : Response
+    public function show(Trick $trick, $page, ChatRepository $chatRepository, Request $request) : Response
     {
+        $chat = new Chat();
+        $form = $this->createForm(ChatType::class, $chat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $trick->addChat($chat);
+            $this->objectManager->flush();
+            return $this->redirectToRoute('trick.show',['id' => $trick->getId(), 'page'=> 1]);
+        }
+
        $chats = $chatRepository->findBy(['trick' => $trick], ['date' => 'DESC'], 10, ($page-1)*10);
        $pages = ceil($chatRepository->count(['trick' => $trick])/10);
-        return $this->render('trick/show.html.twig', ['activeMenu' => 'tricks', 'trick' => $trick, 'chats'=>$chats, 'pages' => $pages]);
+
+        return $this->render('trick/show.html.twig', [
+            'activeMenu' => 'tricks',
+            'trick' => $trick,
+            'chats'=>$chats,
+            'pages' => $pages,
+            'form' => $form->createView()
+        ]);
+
     }
 
 }
