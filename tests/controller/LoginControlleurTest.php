108728f1caa708516076d8ef9585502f7c09b7d3
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-08-20 12:54:10 +0200

    Réalisation of fonctional tests for TrickController

--- tests/controller/LoginControlleurTest.php
+++ tests/controller/LoginControlleurTest.php
@@ -14,19 +14,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class LoginControlleurTest extends WebTestCase
 {
     /**
+     * @var
+     */
+    private $client;
+
+    /**
+     * @var
+     */
+    private $crawler;
+
+    /**
      * Test de connexion avec bon identifiant utilisateur
      */
     public function testGoodAuthentification()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $crawler = $client->submitForm('Valider', [
-            '_username' => 'user5@gmail.com',
-            '_password' => 'user5'
-        ]);
-        $crawler = $client->followRedirect();
-        $this->assertSame(302, $client->getResponse()->getStatusCode(), "echec de l'authentification");
-        echo $client->getResponse()->getContent();
+        $this->client = static::createClient();
+        $this->login('user5@gmail.com', 'user5');
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
+        echo $this->client->getResponse()->getContent();
     }
 
     /**
@@ -34,14 +39,25 @@ class LoginControlleurTest extends WebTestCase
      */
     public function testBadAuthentification()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $crawler = $client->submitForm('Valider', [
-            '_username' => 'toto@gmail.com',
-            '_password' => 'test'
-        ]);
-        $crawler = $client->followRedirect();
-        $this->assertSame(1, $crawler->filter('html .alert:contains("Identifiants invalides.")')->count());
-        echo $client->getResponse()->getContent();
+        $this->client = static::createClient();
+        $this->login('badUser@gmail.com', 'badPassword');
+        $this->assertSame(1, $this->crawler->filter('html .alert:contains("Identifiants invalides.")')->count());
+        echo $this->client->getResponse()->getContent();
+    }
+
+    private function login($user, $password)
+    {
+        $this->crawler = $this->client->request('GET', '/login');
+        $this->hydrateForm('Valider', ['_username' => $user, '_password' => $password]);
+    }
+
+    /**Remplissage du formulaire
+     * @param $button
+     * @param $inputs
+     */
+    private function hydrateForm($button, $inputs)
+    {
+        $this->crawler = $this->client->submitForm($button, $inputs);
+        $this->crawler = $this->client->followRedirect();
     }
 }
